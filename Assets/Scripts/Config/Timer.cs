4a247dde86a6def1fbb8a9bb2265e81b664244ef
Boltairex <pjopl62@gmail.com>
2019-12-07 15:36:47 +0100

    GUI update

---
+++ Assets/Scripts/Config/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    GameClock GC;
+    
+    void Start()
+    {
+        GC = GameObject.Find("GameClock").GetComponent<GameClock>();
+    }
+
+
+    void Update()
+    {
+        string a;
+        string b;
+        string c;
+
+        if (GC.GSTime > 9)
+        { a = null; }
+        else
+        { a = "0"; }
+
+        if (GC.GMTime > 9)
+        { b = null; }
+        else
+        { b = "0"; }
+
+        if (GC.GHTime == 0)
+
+        { gameObject.GetComponent<TextMeshProUGUI>().text = b + GC.GMTime + ":" + a + GC.GSTime; }
+        else
+        { gameObject.GetComponent<TextMeshProUGUI>().text = GC.GHTime + ":" + b + GC.GMTime + ":" + a + GC.GSTime; }
+
+    }
+}
