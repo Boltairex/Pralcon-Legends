ec2f155f911e050554d0946c5158d85ff7c3105e
uzer <defens123@wp.pl>
2019-12-08 00:48:44 +0100

    Dodano menu

---
+++ Assets/Scripts/Menu/ButtonHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public GameObject main, options;
+    bool isMainMenu = true;
+
+    //Tutaj znajduja sie funkcje uzywane przez przyciski
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+    }
+
+    public void OptionsButton()
+    {
+        if (isMainMenu == true)
+        {
+            main.SetActive(false);
+            options.SetActive(true);
+            isMainMenu = false;
+        }  
+        else
+        {
+            main.SetActive(true);
+            options.SetActive(false);
+            isMainMenu = true;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
