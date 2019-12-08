fad935aaa0afe76e696146f077424fcf6498f009
uzer <defens123@wp.pl>
2019-12-08 15:51:31 +0100

    Dodano sklep (0.1v)

--- Assets/Scripts/Ingame/PlayerController.cs
+++ Assets/Scripts/Ingame/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(1))
         {
             ray = cam.ScreenPointToRay(Input.mousePosition);
 
