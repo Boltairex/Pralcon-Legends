048864a0bded8d2dcee83916435c6835ce1c4ea8
Boltairex <pjopl62@gmail.com>
2019-12-15 16:08:36 +0100

    Update Menu

---
+++ Assets/Scripts/Menu/MenuGUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGUI : MonoBehaviour
+{
+    public GameObject Buttons;
+    public GameObject List;
+    public GameObject IA;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Close") && IA.activeInHierarchy)
+        {
+            IA.SetActive(false);
+        }
+    }
+
+    public void ListOpen()
+    {
+        Buttons.SetActive(false);
+        List.SetActive(true);
+    }
+
+    public void ListClose()
+    {
+        Buttons.SetActive(true);
+        List.SetActive(false);
+    }
+
+    public void InputArea()
+    {
+        IA.SetActive(true);
+    }
+}
