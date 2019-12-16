ab2f3e321c218fe4d7088acb58ed6411c23b06ba
Boltairex <pjopl62@gmail.com>
2019-12-16 19:06:47 +0100

    Mdły Networking

--- Assets/Network.cs
+++ Assets/Network.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Lidgren;
+using TMPro;
 
 public class Network : MonoBehaviour
 {
     public GameObject EIP;
     public GameObject EPort;
+    Lidgren.Network.NetPeerConfiguration config;
 
     public void NetChange()
     {
+        config = new Lidgren.Network.NetPeerConfiguration("RunUp");
+        config.Port = int.Parse(EPort.GetComponent<TMP_InputField>().text);
+        if (config.Port <= 0 || config.Port >= 51000)
+        {
+            EPort.GetComponent<TMP_InputField>().text = "7777";
+        }
+    }
 
+    public void ServerStart()
+    {
+        config.EnableUPnP = true;
+        Lidgren.Network.NetServer server = new Lidgren.Network.NetServer(config);
+        server.Start();
     }
 }
