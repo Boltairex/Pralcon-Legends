baea43f0cc2ae7b9cd499ebe5fd02a54947fabec
Tomasz-Jaworski <sevenfriesstudio@gmail.com>
2019-12-21 11:12:26 +0100

    AA and inRAnge system
    2 simple scripts

---
+++ Assets/Scripts/AA and obj in range/AutoAtack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAtack : MonoBehaviour
+{
+
+    ObjectsInRange inRange;
+
+    public GameObject target;
+
+    void Start()
+    {
+        inRange = GetComponent<ObjectsInRange>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            target = null;
+
+            Debug.Log("Clicked");
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                Debug.Log("Hitted");
+                if (hit.transform.gameObject != gameObject)
+                {
+                    target = hit.transform.gameObject;
+                    Debug.Log(target);
+                }
+            }
+
+
+            //jesli target to enemy to ma wykonać AA, ale jesli jest po za zasięgiem to ma zacząć do niego iść. Należy pamiętać o tym że kliknięcie na mape w ceul poruszania się musi ustawiać target na null.
+        }
+
+    }
+}
---
+++ Assets/Scripts/AA and obj in range/ObjectsInRange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsInRange : MonoBehaviour
+{
+    [Header("Objects Storage")]
+    public Transform minions;
+    public Transform players, creatures, bosses;
+
+    [Header("Objects Lists")]
+    public List<GameObject> minionsList = new List<GameObject>();
+    public List<GameObject> playerList = new List<GameObject>();
+    public List<GameObject> creaturesList = new List<GameObject>();
+    public List<GameObject> bossesList = new List<GameObject>();
+
+    [Header("Config")]
+    public int detectRange;
+    public int autoAttackRange;
+
+    void Update()
+    {
+        UpdateLists();
+    }
+
+    public void UpdateLists()
+    {
+        minionsList.Clear();
+        playerList.Clear();
+        creaturesList.Clear();
+        bossesList.Clear();
+
+        foreach (Transform m in minions)
+        {
+            if(Vector3.Distance(m.position, transform.position) * 100 <= detectRange) minionsList.Add(m.gameObject);
+        }
+
+        foreach (Transform p in players)
+        {
+            if (Vector3.Distance(p.position, transform.position) * 100 <= detectRange) playerList.Add(p.gameObject);
+        }
+
+        foreach (Transform c in creatures)
+        {
+            if (Vector3.Distance(c.position, transform.position) * 100 <= detectRange) creaturesList.Add(c.gameObject);
+        }
+
+        foreach (Transform b in bosses)
+        {
+            if (Vector3.Distance(b.position, transform.position) * 100 <= detectRange) bossesList.Add(b.gameObject);
+        }
+    }
+}
